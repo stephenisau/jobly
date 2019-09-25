faf12486b9d1a36c556b08526af553b9e74cda55
Jennifer Garcia <jen.garcia91@gmail.com>
2019-09-25 15:54:39 -0700

    Adding minor styling. Everything's a button! urgh. I am being good and doing small pushes. It's all a blue, help.

--- frontend/src/companies/CompanyCard.js
+++ frontend/src/companies/CompanyCard.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { Link } from 'react-router-dom';
 
 class CompanyCard extends Component {
   render() {
+    let img = this.props.company.logo_url ?
+      <img src="https://png.pngtree.com/svg/20170307/c02483909c.svg" alt={`${this.props.company.name} logo`} /> : <img src={this.props.company.logo_url} alt={`${this.props.company.name} logo`} />
     return (
-    <div>CompanyCard
-      <pre>{JSON.stringify(this.props, null, 4)}</pre>
-    </div>
+      <Link exact to={`companies/${this.props.company.handle}`}>
+        <div className="Card card mt-3">
+          <div className="card-body">
+            <h6>{this.props.company.name}</h6>
+            <p>{this.props.company.description}</p>
+            {img}
+          </div>
+        </div>
+      </Link>
+
     );
   }
 }
--- frontend/src/jobs/JobCard.js
+++ frontend/src/jobs/JobCard.js
@@ -3,9 +3,13 @@ import React, { Component } from "react";
 class JobCard extends Component {
   render() {
     return (
-    <div>
-      JobCard
-      <pre>{JSON.stringify(this.props)}</pre>
+    <div className="Card card mt-3">
+      <div className="card-body">
+        <h6>{this.props.job.title}</h6>
+        <p>Salary: {this.props.job.salary}</p> 
+        <p>Equity: {this.props.job.equity}</p>
+      </div>
+      <button className="btn btn-danger">Apply</button>
     </div>
     );
   }
