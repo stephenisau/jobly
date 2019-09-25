b31eb91a7db0e7eb70bfa4da538170e32a85f6c3
stephenau93 <stephenau93@gmail.com>
2019-09-25 13:40:14 -0700

    added navlink logic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import NavBar from './NavBar';
 import Routes from './routes/Routes';
 import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="container-fluid">
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
         <BrowserRouter>
           <NavBar />
           <Routes />
         </BrowserRouter>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/NavBar.js
+++ frontend/src/NavBar.js
@@ -3,15 +3,21 @@ import { NavLink } from 'react-router-dom';
 
 class NavBar extends Component {
   render() {
+    const navLinks =
+      localStorage.getItem('username') ?
+        <div>
+          <NavLink exact to='/companies'>Companies</NavLink>
+          <NavLink exact to='/jobs'>Jobs</NavLink>
+          <NavLink exact to='/profile'>Profile</NavLink>
+          <NavLink exact to='/logout'>Logout</NavLink>
+        </div> : 
+        <div><NavLink exact to='/login'>Login</NavLink></div>
+
     return (
       <div>
         <nav className="navbar">
-            <NavLink exact to='/'>Jobly</NavLink>
-            <NavLink exact to='/login'>Login</NavLink>
-            <NavLink exact to='/logout'>Logout</NavLink>
-            <NavLink exact to='/companies'>Companies</NavLink>
-            <NavLink exact to='/jobs'>Jobs</NavLink>
-            <NavLink exact to='/profile'>Profile</NavLink>
+          <NavLink exact to='/'>Jobly</NavLink>
+          {navLinks}
         </nav>
       </div>
     );
