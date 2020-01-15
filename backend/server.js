969f8315768d05376155c09a3e9bb1d47cbcc6f8
stephenau93 <stephenau93@gmail.com>
2020-01-15 01:24:08 -0800

    Add ping to keep heroku alive

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 /** Start server for jobly. */
 
+var http = require("http");
+
+setInterval(function() {
+    http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
 
 const app = require('./app');
 const { PORT } = require("./config");
