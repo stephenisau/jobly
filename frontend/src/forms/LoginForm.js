24565a14cb95a5eaa11388c03840d75c8ea395bb
stephenau93 <stephenau93@gmail.com>
2020-02-05 12:08:24 -0800

    Add demo login

--- frontend/src/forms/LoginForm.js
+++ frontend/src/forms/LoginForm.js
@@ -10,7 +10,21 @@ class LoginForm extends Component {
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.demoLogin = this.demoLogin.bind(this);
   }
+
+  demoLogin(evt) {
+    evt.preventDefault();
+    let username = "patrickstar";
+    let password = "password";
+    this.setState({
+      username,
+      password
+    });
+    debugger;
+    this.props.signIn(this.state);
+  }
+
   handleChange(evt) {
     this.setState({
       [evt.target.name]: evt.target.value
@@ -21,6 +35,7 @@ class LoginForm extends Component {
     evt.preventDefault()
     this.props.signIn(this.state);
   }
+  
   render() {
     return (
       <div className="card">
@@ -47,6 +62,7 @@ class LoginForm extends Component {
                 className="form-control" />
             </div>
             <button className="btn btn-primary mt-2" type="submit">Submit</button>
+            <button className="btn btn-info mt-2 ml-2" type="button" onClick={this.demoLogin}> Demo </button>
           </form>
         </div>
       </div>
