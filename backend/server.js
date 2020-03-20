d50b50c852993b6581c046639838a5147bcc13ae
stephenau93 <stephenau93@gmail.com>
2020-03-20 14:53:42 -0700

    removed debugger

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,11 @@
 /** Start server for jobly. */
 
+var http = require("http");
+
+// setInterval(function() {
+//     http.get(`http://jobly-app-backend.herokuapp.com`);
+// }, 300000); // every 5 minutes (300000)
+
 const app = require('./app');
 const { PORT } = require("./config");
 
