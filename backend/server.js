b5d007f14d473dcaf60cf645788816be1ae5d06d
stephenau93 <stephenau93@gmail.com>
2020-03-20 00:20:00 -0700

    Remove code to ping backend every 5 minutes...

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,5 @@
 /** Start server for jobly. */
 
-var http = require("http");
-
-setInterval(function() {
-    http.get("http://<your app name>.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
-
 const app = require('./app');
 const { PORT } = require("./config");
 
